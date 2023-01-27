1e7aac082cfa9b82998501ed55c399f09b4ed730
Saket2766 <saketsamarth27@gmail.com>
2023-01-27 14:40:58 +0530

    new file:   .gitignore 	new file:   server/.gitignore 	new file:   server/package-lock.json 	modified:   server/package.json 	new file:   server/server.js 	modified:   studio/package-lock.json 	modified:   studio/package.json 	new file:   studio/schemas/conversationsSchema.js 	modified:   studio/schemas/index.js 	new file:   studio/schemas/userSchema.js

--- studio/schemas/index.js
+++ studio/schemas/index.js
@@ -1,1 +1,4 @@
-export const schemaTypes = []
+import { userSchema } from "./userSchema"
+import { conversationsSchema } from "./conversationsSchema"
+
+export const schemaTypes = [userSchema,conversationsSchema]
