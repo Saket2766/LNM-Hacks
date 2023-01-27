420c8948a05841c187e607dec7d38b8b576184f1
Saket2766 <saketsamarth27@gmail.com>
2023-01-27 23:28:59 +0530

    modified:   client/components/ConversationList.js 	modified:   client/pages/index.js 	modified:   client/styles/ConversationList.module.css 	client/public/icons/

--- client/components/ConversationList.js
+++ client/components/ConversationList.js
@@ -1,9 +1,16 @@
 import react from "react";
+import styles from "../styles/ConversationList.module.css"
+import { useEffect,useState } from "react";
+import Image from "next/image";
+import nitro from '../public/icons/nitro.svg'
+
 
 const ConversationList = () => {
     return (
-        <div>
-            ConversationList
+        <div className={styles.conversations}>
+            <div className={styles.conversationListTop}>
+                <input type ="search" placeholder="Find or start a conversation"/>
+            </div>
         </div>
     )
 }
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,6 @@
 import styles from '../styles/Home.module.css'
 import Sidebar from '@/components/sidebar'
+import ConversationList from '@/components/ConversationList'
 
 
 export default function Home() {
@@ -7,7 +8,7 @@ export default function Home() {
     <div className ={styles.wrapper}>
       <Sidebar />
       <div className={styles.main}> 
-        Conversations List Chat View
+        <ConversationList/>
       </div>
     </div>
   )
