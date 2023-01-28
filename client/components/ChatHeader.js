64c4da09d2f8da2e517a9382d381a85118e1340c
Saket2766 <saketsamarth27@gmail.com>
2023-01-28 14:23:54 +0530

    modified:   client/components/ChatHeader.js 	new file:   client/public/eth.png

--- client/components/ChatHeader.js
+++ client/components/ChatHeader.js
@@ -3,11 +3,17 @@ import phone from "../public/icons/phone.svg";
 import help from "../public/icons/help.svg";
 import pin from "../public/icons/pin.svg";
 import at from "../public/icons/at.svg";
+import video from "../public/icons/video.svg";
+import personPlus from "../public/icons/person-plus.svg";
 import Image from "next/image";
 import styles from "../styles/ChatHeader.module.css";
-// import ethLogo from "../public/ethLogo.png";
+import ethLogo from "../public/eth.png";
+
+const currentAccount = "0x86da4B54934Bfd877CF2f85368074096F5662fe0"
+
 const ChatHeader = () =>{
-    <div className={styles.chatHeader}>
+
+    return (<div className={styles.chatHeader}>
         <div className ={styles.roomNameContainer}>
             <Image
                 heigth = {20}
@@ -19,11 +25,73 @@ const ChatHeader = () =>{
             <h3 className={styles.title}>Room Name</h3>
             <div className={styles.chatHeaderStatus} id='online' />
         </div>
-        <div className= {styles.connectWallet} onClick ={()=>connectWallet()}>
-            Connect Wallet
+        {currentAccount ? (
+        <div className={styles.connectedWallet}>
+          <Image src={ethLogo} height={20} width={15} alt='ethLogo' />
+          <span className={styles.separator}>{'|'}</span>
+          {currentAccount.slice(0, 6)}...{currentAccount.slice(39)}
         </div>
-        
+      ) : (
+        <div className={styles.connectWallet} onClick={() => connectWallet()}>
+          Connect Wallet
+        </div>
+      )}
+
+<div className={styles.headerIconsContainer}>
+        <div className={styles.headerItem}>
+          <Image
+            height={25}
+            width={25}
+            src={phone}
+            className={styles.svg}
+            alt=''
+          />
+        </div>
+        <div className={styles.headerItem}>
+          <Image
+            height={25}
+            width={25}
+            src={video}
+            className={styles.svg}
+            alt=''
+          />
+        </div>
+      </div>
+      <div className={styles.headerItem}>
+        <Image height={25} width={25} src={pin} className={styles.svg} alt='' />
+      </div>
+      <div className={styles.headerItem}>
+        <Image
+          height={25}
+          width={25}
+          src={personPlus}
+          className={styles.svg}
+          alt=''
+        />
+      </div>
+      <div className={styles.headerItem}>
+        <input type='search' placeholder='Search' />
+      </div>
+      <div className={styles.headerItem}>
+        <Image
+          height={25}
+          width={25}
+          src={inbox}
+          className={styles.svg}
+          alt=''
+        />
+      </div>
+      <div className={styles.headerItem}>
+        <Image
+          height={25}
+          width={25}
+          src={help}
+          className={styles.svg}
+          alt=''
+        />
     </div>
+        
+</div>)
 }
 
 export default ChatHeader;
