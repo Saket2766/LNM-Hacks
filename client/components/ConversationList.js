cea673d1d0406f7a9e0796d3bdc51aba5b15513b
Saket2766 <saketsamarth27@gmail.com>
2023-01-29 12:07:27 +0530

    modified:   client/components/ConversationList.js 	modified:   client/components/HuddleVid.js

--- client/components/ConversationList.js
+++ client/components/ConversationList.js
@@ -20,84 +20,67 @@ const dummyDms = [{
 
 
 const ConversationList = () => {
-    const [dms,setDms] = useState([])
+    const [dms, setDms] = useState([])
 
-    async function convListfix (){
+    async function convFix(){
         try {
-            const response = await fetch(
-              `${process.env.NEXT_PUBLIC_API_URL}/getdms`,
-            )
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/getdms`)
       
-            const data = await response.json()
-            setChannels(data)
-            router.push(`?channel=${data[0].roomId}&name=${data[0].roomName}`)
+            setDms(await response.json())
           } catch (error) {
             console.error(error)
           }
-    } 
-
-    useEffect( () => {
-        convListfix
-      }, [])
-
+    }
+  
+    useEffect(() => {
+      convFix()
+    }, [])
     return (
-        <div className={styles.conversations}>
-            <div className={styles.conversationListTop}>
-                <input type ="search" placeholder="Find or start a conversation"/>
-            </div>
-
-            <div className={styles.conversationsContainer}>
-                <div className={styles.elementsContainer}>
-                    <div className={styles.svgContainer}>
-                        <Image
-                            height={25}
-                            width={25}
-                            src = {friends}
-                            className = {styles.svg}
-                            alt = 'friends'
-                        />
-                    </div>
-                    <p>
-                        Friends
-                    </p>
-                </div>
-
-                <div className={styles.elementsContainer}>
-                    <div className={styles.svgContainer}>
-                        <Image
-                            height={25}
-                            width={25}
-                            src = {nitro}
-                            className = {styles.svg}
-                            alt = 'nitro'
-                        />
-                    </div>
-                    <p>
-                        Nitro
-                    </p>
-                </div>
-
-                <div className={styles.dmTitle}>
-                    DIRECT MESSAGES
-                </div>
-
-                {dms.map( (dm,index) => (
-                    <DMCard
-                        key = {index}
-                        name = {dm.name}
-                        id = {dm.id}
-                        avatar = {
-                            dm.avatar ||
-                            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3OCSMFI'
-                        }
-                        status = 'online'
-                    />
-                ))}
-            </div>
-
-            
+      <div className={styles.conversations}>
+        <div className={styles.conversationListTop}>
+          <input type='search' placeholder='Find or start a conversation' />
         </div>
+        <div className={styles.conversationsContainer}>
+          <div className={styles.elementsContainer}>
+            <div className={styles.svgContainer}>
+              <Image
+                height={25}
+                width={25}
+                src={friends}
+                className={styles.svg}
+                alt='friends'
+              />
+            </div>
+            <p>Fiends</p>
+          </div>
+          <div className={styles.elementsContainer}>
+            <div className={styles.svgContainer}>
+              <Image
+                height={25}
+                width={25}
+                src={nitro}
+                className={styles.svg}
+                alt='nitro'
+              />
+            </div>
+            <p>Nitro</p>
+          </div>
+          <div className={styles.dmTitle}>DIRECT MESSAGES</div>
+          {dms.map((dm, index) => (
+            <DmCard
+              key={index}
+              name={dm.name}
+              id={dm.id}
+              avatar={
+                dm.avatar ||
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3OCSMFIW5fZ3vSN6yGpD-w-6SsL2_ZPA_sw&usqp=CAU'
+              }
+              status='online'
+            />
+          ))}
+        </div>
+      </div>
     )
-}
-
-export default ConversationList;
+  }
+  
+  export default ConversationList
--- client/components/HuddleVid.js
+++ client/components/HuddleVid.js
@@ -4,4 +4,5 @@ const huddleClient = getHuddleClient("5f4b7a2a5a860e53725b4ecc9774b4aa3b5905bfce
 const HuddleVid = () => {
 }
 
+
 export default HuddleVid
